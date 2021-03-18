f0bd37a40d6927fc80d84000ff8c0228e0fa4689
Sermuonelis <vita.macii@gmail.com>
2021-03-18 12:44:00 +0200

    added hungry promt, programyte veikia

--- hungry.py
+++ hungry.py
@@ -4,4 +4,8 @@ Created on Thu Mar 18 12:14:51 2021
 
 @author: vitam
 """
-print('I am hungry :(((')
+hungry = input('are you hungry?')
+if hungry == 'yes':
+    print('eat smth dude')
+else:
+    print('do your homework')
